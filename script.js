7bf4836a7bf349debb3577ab270e267325e27151
Rayshawn <rayshawnbray@gmail.com>
2022-06-22 19:55:43 -0400

    Create addToThePage Function and successfully test it

--- script.js
+++ script.js
@@ -24,3 +24,34 @@ function addBookToLibrary(title, author, pages, read) {
 addBookToLibrary('The Cat in the Hat', 'Dr. Seuss', 20, 'read');
 addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'not read yet');
 console.log(myLibrary);
+
+
+// Function Add books to Page
+let container = document.querySelector('.container');
+
+function addBookToPage() {
+    // loop over the myLibrary array
+    for (let i = 0; i < myLibrary.length; i++) {
+        console.log(myLibrary[i]);
+        // create a div for card, h1 for title, h2 for author, h3 for pages, and p for read
+        const cardDiv = document.createElement('div');
+        const h1 = document.createElement('h1');
+        const h2 = document.createElement('h2');
+        const h3 = document.createElement('h3');
+        const p = document.createElement('p');
+
+        // add the content to h1, h2, h3, p
+        h1.textContent = myLibrary[i].title;
+        h2.textContent = myLibrary[i].author;
+        h3.textContent = myLibrary[i].pages;
+        p.textContent = myLibrary[i].read;
+
+        // append elements to div
+        cardDiv.append(h1, h2, h3, p);
+
+        // append elements to the page
+        container.appendChild(cardDiv);
+    }
+};
+
+addBookToPage();
